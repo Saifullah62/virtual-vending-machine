2d6ce1b0110bb42c427d712034a55faa43d75381
GotThatData <bwd@ugherty.net>
2024-11-17 11:15:04 -0500

    Create vvm-vending-machine-template.php

---
+++ templates/vvm-vending-machine-template.php
@@ -0,0 +1,5 @@
+<div class="vvm-vending-machine">
+    <h2>Virtual Vending Machine</h2>
+    <p>Insert your coin to buy a Sharepack!</p>
+    <button id="vvm-buy-button">Buy a Sharepack</button>
+</div>
