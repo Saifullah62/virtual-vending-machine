ba919b2bec7c24374f372cdd0cec49ce7497bd7d
GotThatData <bwd@ugherty.net>
2024-11-17 11:06:33 -0500

    Update vvm-functions.php

--- includes/vvm-functions.php
+++ includes/vvm-functions.php
@@ -1,1 +1,12 @@
 touch includes/vvm-functions.php
+<?php
+// Function to create custom WooCommerce product categories for Sharepacks.
+function vvm_create_sharepack_categories() {
+    if (class_exists('WooCommerce')) {
+        wp_insert_term('Common', 'product_cat', array('description' => 'Common Sharepack', 'slug' => 'common-sharepack'));
+        wp_insert_term('Rare', 'product_cat', array('description' => 'Rare Sharepack', 'slug' => 'rare-sharepack'));
+        wp_insert_term('Ultra-Rare', 'product_cat', array('description' => 'Ultra-Rare Sharepack', 'slug' => 'ultra-rare-sharepack'));
+        wp_insert_term('Grail', 'product_cat', array('description' => 'Grail Sharepack', 'slug' => 'grail-sharepack'));
+    }
+}
+add_action('init', 'vvm_create_sharepack_categories');
