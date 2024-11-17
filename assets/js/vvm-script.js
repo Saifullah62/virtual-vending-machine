4e65c4b1def91ab04d8b5a8d27e3071c2f07e0c3
GotThatData <bwd@ugherty.net>
2024-11-17 11:09:00 -0500

    Create vvm-script.js

---
+++ assets/js/vvm-script.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(document).ready(function () {
+        // Example: Trigger an alert when the buy button is clicked.
+        $('#vvm-buy-button').click(function () {
+            alert('You have initiated a purchase of a Sharepack!');
+            // Later, we will add more code here to connect to the WooCommerce checkout process.
+        });
+    });
+})(jQuery);
